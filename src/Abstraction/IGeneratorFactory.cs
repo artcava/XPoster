744c07d348e8a00f1b7e41e3cbeb53fc96ec7f4d
Marco Cavallo <cavallo.marco@gmail.com>
2026-06-13 22:40:24 +0200

    refactor(#156): delete obsolete Generator-named files
    Remove files superseded by the Orchestrator rename:
    - IGenerator.cs -> IOrchestrator.cs
    - IGeneratorFactory.cs -> IOrchestratorFactory.cs
    - BaseGenerator.cs -> BaseOrchestrator.cs
    - FeedGenerator.cs -> FeedOrchestrator.cs
    - PowerLawGenerator.cs -> PowerLawOrchestrator.cs
    - NoGenerator.cs -> NoOrchestrator.cs
    - GeneratorFactory.cs -> OrchestratorFactory.cs

--- src/Abstraction/IGeneratorFactory.cs
+++ src/Abstraction/IGeneratorFactory.cs
@@ -1,14 +1,2 @@
-namespace XPoster.Abstraction;
-
-/// <summary>
-/// Creates the appropriate <see cref="BaseGenerator"/> instance for the current time slot
-/// according to the configured posting schedule.
-/// </summary>
-public interface IGeneratorFactory
-{
-    /// <summary>
-    /// Resolves and returns the generator that matches the current hour of the day.
-    /// </summary>
-    /// <returns>A fully initialised <see cref="BaseGenerator"/> ready to generate and send a post.</returns>
-    BaseGenerator Generate();
-}
+// This file has been removed as part of the Generator -> Orchestrator rename (issue #156).
+// See IOrchestratorFactory.cs
